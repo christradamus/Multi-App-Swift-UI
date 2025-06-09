4ce781f4f0eb82184bff17ca8f35dbda2cf4dc89
Christian Pérez Flores <e_cperezf@smu.cl>
2025-06-09 13:28:45 -0400

    Initial Commit

---
+++ firstAppUI/firstAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppUIApp.swift
+//  firstAppUI
+//
+//  Created by Christian Perez flores on 09-06-25.
+//
+
+import SwiftUI
+
+@main
+struct firstAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
